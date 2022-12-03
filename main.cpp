42e402cb15b1a488a84b62655a64820e53983bef
Mishachkld <mishachkld@gmail.com>
2022-12-03 20:31:28 +0200

    Реализовано выделение коммбинаций

--- main.cpp
+++ main.cpp
@@ -2,137 +2,146 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <cstring>
 
 
 #define STR_SIZE 50
-#define SIZE_ALPH  26
-std::string FILE_PATH_IN = "Text.txt";
-std::string FILE_PATH_OUT = "output.txt";
+#define SIZE_ALPH 26
+std::string FILE_PATH_IN = "D:\\C++Project\\Additional3\\text.txt";
+std::string FILE_PATH_OUT = "D:\\C++Project\\Additional3\\output.txt";
 std::string exceptions = " ,;\n'"".?!:(){}[]*&^%$#@-_=+\\|/0123456789";
 std::string ALL_LITTERS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-std::string changed_litters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 std::string ALL_LITTERS_LOWER = "abcdefghijklmnopqrstuvwxyz";
-char maxLitters[4];
-int N;
+std::string upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string downLetters = "abcdefghijklmnopqrstuvwxyz";
+std::string word;
 
-void outArray(std::string *myString);
+
+
+
+void searchMax(int *counter);
+
+std::string remover(std::string str);
+
 
 void outArrayS(int *counter) {
-	for (int i = 0; i < SIZE_ALPH; i++) {
-		if (counter[i] != 0)
+    for (int i = 0; i < SIZE_ALPH; i++) {
+        if (counter[i] != 0)
 
-			std::cout << changed_litters[i] << " - " << counter[i] << std::endl;
-	}
-
-
-}
-
-void searchMax(int *counter) {
-	for (int i = 0; i < SIZE_ALPH; i++) {
-		for (int j = i; j < SIZE_ALPH; j++) {
-			if (counter[i] < counter[j]) {
-				std::swap(counter[i], counter[j]);
-				std::swap(changed_litters[i], changed_litters[j]);
-			}
-		}
-
-	}
-	changed_litters.resize(4);
+            std::cout << upLetters[i] << " - " << counter[i] << std::endl;
+    }
 
 
 }
 
-void chekForLetters(std::string &word) {
-	for (int i = 0; i < 4; i++) {
-		for (int j = i+1; j < 3; j++) {
-			char str = changed_litters[i] + changed_litters[j];
-			if (word.find(str) != std::string::npos) {
-				word[word.find(str)] = toupper(word[word.find(str)]);
-				word[word.find(str) + 1] = toupper(word[word.find(str)+1]);
-				std::cout << word << std::endl;
-				std::cout << word.find(str);
+void changeWord(int index, std::string info){
+    word[index] = (char) toupper(word[index] );
+    word[index + 1] = (char) toupper(word[index+ 1]);
+    word += "(";
+    word.push_back(info[0]);
+    word += ",";
+    word.push_back(info[1]);
+    word += ")";
+}
 
-			}
-				
-		}
+bool checkForLetters(std::string &wordF) {
+    bool flag = false;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::string strUp;
+            strUp.push_back(upLetters[j]);
+            strUp.push_back(upLetters[i]);
+            std::transform(wordF.begin(), wordF.end(), wordF.begin(),
+                           toupper);
+            if ((wordF.find(strUp) != std::string::npos) and (upLetters[j] != upLetters[i])) {
+                std::cout << strUp << std::endl;
+                flag = true;
+                int index = wordF.find(strUp);
+                changeWord(index, strUp);
 
-	}
+            }
 
+        }
 
+    }
+
+    return flag;
 }
 
 
 void writeNewText() {
-	std::ifstream file(FILE_PATH_IN);
-	std::string word;
-	while (!file.eof()) {
-		file >> word;
-		chekForLetters(word);
-		std::cout << word << std::endl;
-		
+    std::ifstream file(FILE_PATH_IN);
+    std::string  upperWord;
+    while (!file.eof()) {
+        file >> word;
+        upperWord = word;
 
-	}
+        checkForLetters(upperWord);
+        std::cout << word << std::endl;
+
+
+    }
 
 
 }
 
 int counts(std::string *s, std::string what, int j) {
-	for (int i = 0; i < j; ++i) {
-		if (s[i] == what)
-			return false;
-	}
-	return true;
+    for (int i = 0; i < j; ++i) {
+        if (s[i] == what)
+            return false;
+    }
+    return true;
 }
 
 
-std::string remover(std::string str) {
-	std::string ret = "";
-	for (char i : str) {
-		if (exceptions.find(i) == std::string::npos)
-			ret += i;
-	}
-	return ret;
-}
 
 
 int main() {
-	setlocale(0, "");
-	std::ifstream file(FILE_PATH_IN);
-	int counterString[STR_SIZE] = { 0 };
-	std::string elements;
+    setlocale(0, "");
+    std::ifstream file(FILE_PATH_IN);
+    int counterString[STR_SIZE] = {0};
+    std::string elements;
 
-	while (getline(file, elements)) {
-		for (int i = 0; i < ALL_LITTERS_UPPER.length(); i++) {
-			counterString[i] += std::count(elements.begin(), elements.end(), ALL_LITTERS_UPPER[i]);
-			counterString[i] += std::count(elements.begin(), elements.end(), ALL_LITTERS_LOWER[i]);
-		}
-	}
+    while (getline(file, elements)) {
+        for (int i = 0; i < ALL_LITTERS_UPPER.length(); i++) {
+            counterString[i] += std::count(elements.begin(), elements.end(), ALL_LITTERS_UPPER[i]);
+            counterString[i] += std::count(elements.begin(), elements.end(), ALL_LITTERS_LOWER[i]);
+        }
+    }
 
-	file.close();
-	searchMax(counterString);
-	//std::cout << changed_litters << std::endl;
-	//outArrayS(counterString);
-	writeNewText();
+    file.close();
+    searchMax(counterString);
+    //std::cout << upLetters << std::endl;
+    //outArrayS(counterString);
+    writeNewText();
 
 
-
-	system("pause");
-	return 0;
+    return 0;
 }
 
+void searchMax(int *counter) {
+    for (int i = 0; i < SIZE_ALPH; i++) {
+        for (int j = i; j < SIZE_ALPH; j++) {
+            if (counter[i] < counter[j]) {
+                std::swap(counter[i], counter[j]);
+                std::swap(upLetters[i], upLetters[j]);
+                std::swap(downLetters[i], downLetters[j]);
+            }
+        }
 
-void outArray(std::string *myString) {
-	std::ofstream file(FILE_PATH_OUT, std::ios::out);
-	for (int i = 0; i < N; i++) {
-		if (!myString[i].empty()) {
-			std::cout << myString[i] << " ";
-			//            file << myString[i] << std::endl;
-		}
-	}
-	std::cout << std::endl;
-
-	file.close();
-
+    }
+    upLetters.resize(4);
 
 
 }
+
+std::string remover(std::string str) {
+    std::string ret = "";
+    for (char i: str) {
+        if (exceptions.find(i) == std::string::npos)
+            ret += i;
+    }
+    return ret;
+}
+
+
